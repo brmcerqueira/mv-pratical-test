246cd69ccfabce73656f168a2d4910fe02b95974
Bruno Cerqueira <brmcerqueira@gmail.com>
2018-07-16 16:40:06 -0300

    Ajuste

--- single-sign-on/src/main/java/com/singlesignon/presentation/Application.java
+++ single-sign-on/src/main/java/com/singlesignon/presentation/Application.java
@@ -2,8 +2,15 @@ package com.singlesignon.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@ComponentScan({
+    "com.singlesignon.presentation",
+    "com.singlesignon.business"
+})
 public class Application {
 
     public static void main(String[] args) {
--- single-sign-on/src/main/java/com/singlesignon/presentation/controllers/AuthenticationController.java
+++
@@ -1,7 +0,0 @@
-package com.singlesignon.presentation.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AuthenticationController {
-}
